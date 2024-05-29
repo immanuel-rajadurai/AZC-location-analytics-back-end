b776497ce0e77249e635926df56abb84a9e26673
Arjan Bedi <161597359+RapidShotzz@users.noreply.github.com>
2024-05-29 14:34:01 +0100

    Updated area.py to define an inheritance hierarchy of Area, Restaurant and Exhibit classes
    Superclass 'Area' contains the geofence wherein locations can be removed or added.
    
    Restaurant class inherits from the Area class and contains information on the restaurant type and menus.
    
    Exhibit class also inherits from the Area class and contains information on animals present and whether the region is open or closed.

--- src/area.py
+++ src/area.py
@@ -15,3 +15,70 @@ The Exhibit subclass must contain information such as animal, whether or not it 
 that you feel is relevant
 
 """
+
+from location import Location
+
+class Area:
+
+  def __init__(self,name,geofence): # geofence is a list of location objects
+    self.name = name
+    self.geofence = geofence
+
+  def add_location(self,location): # adds a location to the geofence
+    self.geofence.append(location)
+
+  def remove_location(self,location): # removes a location from the geofence
+    self.geofence.remove(location)
+
+  def get_geofence(self):
+    return self.geofence
+
+
+class Restaurant(Area):
+
+  def __init__(self,name,geofence,restaurant_type,menu=None):
+    super().__init__(name,geofence) # refers to the Area constructor (superclass) to determine the name and geofence
+    self.restaurant_type = restaurant_type
+    self.menu = menu if menu is not None else []
+
+  def set_restaurant_type(self,restaurant_type): # sets the restaurant type
+    self.restaurant_type = restaurant_type
+
+  def get_restaurant_type(self): # gets the type of restaurant e.g. cafe, takeaway etc
+    return self.restaurant_type
+
+  def add_menu_item(self,item): # adds an item to the restaurant menu
+    self.menu.append(item)
+
+  def get_menu(self): # gets the menu of a restaurant
+    return self.menu
+
+
+class Exhibit(Area):
+
+  def __init__(self,name,geofence,animal,is_open):
+    super().__init__(name,geofence)
+    self.animal = animal
+    self.is_open = is_open
+
+  def set_animal(self,animal): # sets the animal in an exhibit
+    self.animal = animal
+
+  def get_animal(self): # gets an animal in the exhibit
+    return self.animal
+
+  def open_exhibit(self): # opens an exhibit
+    self.is_open = True
+
+  def closed_exhibit(self): # closes an exhibit
+    self.is_open = False
+
+  def is_exhibit_open(self): # informs whether the exhibit is open or not
+    return self.open
+                  
+  
+  
+    
+
+ 
+     
